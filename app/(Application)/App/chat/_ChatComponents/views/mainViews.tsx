58ebcc40e6b79d4346b5887ac0c8e290f69e70b0
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-10 13:27:53 +0100

    ``` chore: update project dependencies for improved performance and security ```

--- app/(Application)/App/chat/_ChatComponents/views/mainViews.tsx
+++ app/(Application)/App/chat/_ChatComponents/views/mainViews.tsx
@@ -1,8 +1,6 @@
 import { DropdownMenuGroup, DropdownMenuItem } from "@/components/ui/dropdown-menu"
 import type { MenuItem } from "@/components/myComponents/utilityComponent/types"
 import NewChatSearch from "./newChatComponent"
-import PhoneAndGroup from "./phoneAndGroup"
-import getSearchUsers from "@/actions/api-actions/userAction/getSearchUsers"
 
 interface MainViewProps {
   items: MenuItem[]
@@ -12,11 +10,10 @@ interface MainViewProps {
 
 export const MainView = async ({ items, onItemClick, onUserSelect }: MainViewProps) => {
   // Fetch initial users for server-side rendering
-  const initialUsers = await getSearchUsers("", "1", "5")
 
   return (
     <DropdownMenuGroup>
-      <NewChatSearch initialSearch={initialUsers} onUserSelect={onUserSelect} />
+      <NewChatSearch  onUserSelect={onUserSelect} />
 
       {items.map((item) => (
             <DropdownMenuItem
