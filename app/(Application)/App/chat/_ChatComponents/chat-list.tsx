e047475259322b1cd72f559dda7784cf9b69a3d2
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-12 13:43:23 +0100

    fix: update ChatList component to conditionally render group, friend, and favourite conversations

--- app/(Application)/App/chat/_ChatComponents/chat-list.tsx
+++ app/(Application)/App/chat/_ChatComponents/chat-list.tsx
@@ -3,7 +3,10 @@
 import { useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { SearchBar } from "@/components/myComponents/utilityComponent/SearchBar";
-import { ConversationResponse, useSearchQuery } from "@/actions/api-actions/userAction/getSearch";
+import {
+  ConversationResponse,
+  useSearchQuery,
+} from "@/actions/api-actions/userAction/getSearch";
 import ChatPageHeader from "./chatPageHeader";
 import useDebounce from "@/hooks/utilityHooks/useDebounce";
 import ConversatonListItem from "@/components/myComponents/conversations/conversationListItem";
@@ -17,11 +20,18 @@ export function ChatList() {
   const [searchWord, setSearchWord] = useState("");
   const filterState = ZustanFilterStore((state) => state.filters);
   const debouncedResult = useDebounce(searchWord, 500);
-  const { data, isLoading, isError } = useSearchQuery(debouncedResult, 1, 10, filterState);
+  const { data, isLoading, isError } = useSearchQuery(
+    debouncedResult,
+    1,
+    10,
+    filterState
+  );
 
   const render = (filterData: ConversationResponse | undefined) => {
     if (isLoading) {
-      return <div className="w-full flex items-center">Fetching Conversations</div>;
+      return (
+        <div className="w-full flex items-center">Fetching Conversations</div>
+      );
     }
 
     if (isError || !filterData) {
@@ -29,81 +39,108 @@ export function ChatList() {
     }
 
     if (filterData.directConversations.length < 1) {
-      return <div className="w-full flex items-center">There is no conversation</div>;
+      return (
+        <div className="w-full flex items-center">There is no conversation</div>
+      );
     }
 
     if (filterData.directConversations.length > 0) {
       return (
         <>
-          {filterState.id === "all" ? (<>
-            <div className="w-full flex items-center">
-            <strong className="font-bold">Conversations</strong>
-          </div>
-          <div>
-            {filterData.directConversations.map((convo) => (
-              <ConversatonListItem key={convo.id} conversation={convo} />
-            ))}
-          </div>
-          <div className="w-full flex items-center">
-            <strong className="font-bold">Groups</strong>
-          </div>
-          <div>
-            {filterData?.groupConversations.map((convo) => (
-              <GroupListItem key={convo?.id} conversation={convo} />
-            ))}
-          </div>
+          {filterState.id === "all" ? (
+            <>
+              <div className="w-full flex items-center">
+                <strong className="font-bold">Conversations</strong>
+              </div>
+              <div>
+                {filterData.directConversations.map((convo) => (
+                  <ConversatonListItem key={convo.id} conversation={convo} />
+                ))}
+              </div>
 
-          <div className="w-full flex items-center">
-            <strong className="font-bold">Friends</strong>
-          </div>
-          <div>
-            {filterData.friendConvo.map((convo) => (
-              <FriendListConvo className="" conversation={convo}/>
-            ))}
-          </div>
+              {filterData.groupConversations.length > 0 ? (
+                <>
+                  <div className="w-full flex justify-center items-center">
+                    <strong className="font-bold">Groups</strong>
+                  </div>
+                  <div>
+                    {filterData?.groupConversations.map((convo) => (
+                      <GroupListItem key={convo?.id} conversation={convo} />
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
 
-          <div className="w-full flex items-center">
-            <strong className="font-bold">Friends</strong>
-          </div>
-          <div>
-            {filterData.favouriteConvo.map((convo) => (
-              <FavouriteListConvo className="" conversation={convo}/>
-            ))}
-          </div>
-          
-            </>) : filterState.id == "groups" ? (
-          <>
-          <div className="w-full flex items-center">
-            <strong className="font-bold">Groups</strong>
-          </div>
-          <div>
-            {filterData.groupConversations.map((convo) => (
-              <GroupListItem key={convo?.id} conversation={convo} />
-            ))}
-          </div>
-        </>): filterState.id === "non-contact" ? (<>
-          <div className="w-full flex items-center">
-            <strong className="font-bold">Friends</strong>
-          </div>
-          <div>
-            {filterData.friendConvo.map((convo) => (
-              <FriendListConvo className="" conversation={convo}/>
-            ))}
-          </div>
-        </>) : filterState.id === "favourites" ? (<>
-          <div className="w-full flex items-center">
-            <strong className="font-bold">Friends</strong>
-          </div>
-          <div>
-            {filterData.favouriteConvo.map((convo) => (
-              <FavouriteListConvo className="" conversation={convo}/>
-            ))}
-          </div>
-        </>): (<></>)}
+              {filterData.friendConvo.length > 0 ? (
+                <>
+                  <div className="w-full flex justify-center items-center">
+                    <strong className="font-bold">Friends</strong>
+                  </div>
+                  <div>
+                    {filterData.friendConvo.map((convo) => (
+                      <FriendListConvo className="" conversation={convo} />
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+              {filterData.favouriteConvo.length > 0 ? (
+                <>
+                  <div className="w-full flex justify-center items-center">
+                    <strong className="font-bold">Friends</strong>
+                  </div>
+                  <div>
+                    {filterData.favouriteConvo.map((convo) => (
+                      <FavouriteListConvo className="" conversation={convo} />
+                    ))}
+                  </div>
+                </>
+              ) : (
+                <></>
+              )}
+            </>
+          ) : filterState.id == "groups" ? (
+            <>
+              <div className="w-full flex items-center">
+                <strong className="font-bold">Groups</strong>
+              </div>
+              <div>
+                {filterData.groupConversations.map((convo) => (
+                  <GroupListItem key={convo?.id} conversation={convo} />
+                ))}
+              </div>
+            </>
+          ) : filterState.id === "non-contact" ? (
+            <>
+              <div className="w-full flex justify-center items-center">
+                <strong className="font-bold">Friends</strong>
+              </div>
+              <div>
+                {filterData.friendConvo.map((convo) => (
+                  <FriendListConvo className="" conversation={convo} />
+                ))}
+              </div>
+            </>
+          ) : filterState.id === "favourites" ? (
+            <>
+              <div className="w-full flexjustify-center items-center">
+                <strong className="font-bold">Favourite</strong>
+              </div>
+              <div>
+                {filterData.favouriteConvo.map((convo) => (
+                  <FavouriteListConvo className="" conversation={convo} />
+                ))}
+              </div>
+            </>
+          ) : (
+            <></>
+          )}
         </>
       );
     }
-
 
     return null;
   };
@@ -112,7 +149,11 @@ export function ChatList() {
     <div className="w-full h-full border-r border-gray-200 flex flex-col bg-white">
       <ChatPageHeader />
       <div className="p-4">
-        <SearchBar placeholder="Search or start new chat" value={searchWord} onChange={(value: string) => setSearchWord(value)} />
+        <SearchBar
+          placeholder="Search or start new chat"
+          value={searchWord}
+          onChange={(value: string) => setSearchWord(value)}
+        />
       </div>
       <ScrollArea className="flex-1">
         <div className="space-y-2">{render(data)}</div>
