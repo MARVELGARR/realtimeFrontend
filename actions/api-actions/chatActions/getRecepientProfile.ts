5e2cd7c4eddc6bddf9f0cca57a47ed492ebc0f73
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-17 13:36:38 +0100

    dss

--- actions/api-actions/chatActions/getRecepientProfile.ts
+++ actions/api-actions/chatActions/getRecepientProfile.ts
@@ -5,7 +5,7 @@ type getRecepientProfileProps ={
     image: string
     createdAt:string,
     updatedAt: string
-    friends: Friend[]
+    Friends: Friend[]
     profile: ProfileRecepient
 }
 type Friend={
