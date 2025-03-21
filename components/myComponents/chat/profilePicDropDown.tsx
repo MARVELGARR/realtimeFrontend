0968ff0a7f51faebb11ed0faf49ca50fc72a458a
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-03-21 08:43:22 +0100

    ``` feat: add functionality for group message search and filter options ```

--- components/myComponents/chat/profilePicDropDown.tsx
+++ components/myComponents/chat/profilePicDropDown.tsx
@@ -1,12 +1,10 @@
+import GroupProfile from "@/components/group-profile";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 type ProfilePicDropdownProps = {
   className?: string;
@@ -32,73 +30,22 @@ export function ProfilePicDropdown({
   };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+   
+  <Popover>
+    <PopoverTrigger>
         <Avatar className="cursor-pointer">
           <AvatarImage src={recepientProfilePic!} className="" />
           <AvatarFallback>{`g${
             recepientName ? recepientName[0] + recepientName[-1] : ""
           } f`}</AvatarFallback>
         </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className={cn(className, "w-36")}>
-        <DropdownMenuItem
-          asChild
-          className="hover:bg-green-200"
-        >
-         <DisplaProfilePicDialog recepientProfilePic={recepientProfilePic!} recepientName={recepientName}/>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          
-          className="hover:bg-green-200"
-          asChild
-        >
-          <ProfileCardDialog recepientId={recepientId!} className="" recepientName={recepientName}/>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => handleClick("un-friend")}
-          className="hover:bg-green-200"
-        >
-          Unfriend
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => handleClick("block")}
-          className="hover:bg-green-200"
-        >
-          Block user
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+
+    </PopoverTrigger>
+    <PopoverContent>
+      <GroupProfile/>
+    </PopoverContent>
+  </Popover>
   );
 }
 
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { ProfileCardDialog } from "./profileCard";
 
-export function DisplaProfilePicDialog({
-  recepientProfilePic,
-  recepientName,
-}: {
-  recepientProfilePic: string;
-  recepientName: string;
-}) {
-  return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button className=" w-full flex justify-start pl-2 py-4 align-left border-none  h-5 " variant="outline"> <span className="align-left">view Pic</span> </Button>
-      </DialogTrigger>
-      <DialogContent className="w-[425px] p-0">
-      <AspectRatio ratio={16 / 9} className="bg-muted">
-          <Image
-            src={recepientProfilePic!}
-            alt={recepientName!}
-            fill
-            className="h-full w-full rounded-md object-cover"
-          />
-       </AspectRatio>
-      </DialogContent>
-    </Dialog>
-  );
-}
