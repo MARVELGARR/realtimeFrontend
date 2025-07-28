e8709b635d714a1fbf2588e0d9cddd098447dc23
Marvellous Obatale <75810831+MARVELGARR@users.noreply.github.com>
2025-07-28 08:33:26 +0100

    d

--- configs/socket.ts
+++ configs/socket.ts
@@ -2,4 +2,4 @@
 import io from "socket.io-client";
 
 
-export const socket = io(`ws://${process.env.NEXT_PUBLIC_API_URL_WBESOCKET}`);
+export const socket = io(`wss://${process.env.NEXT_PUBLIC_API_URL_WBESOCKET}`);
